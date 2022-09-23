8457093b92cc03c672a203e21ecb6ec4e29412d6
Fernando <fbanarescarrasco@gmail.com>
2022-09-22 22:42:27 -0300

    correccion 1

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.get('/fyh',(req, res) => {
 
 const PORT = process.env.PORT || 8080
 
-const server = app.listen(PORT, (err, res) => {
+const server = app.listen(PORT, () => {
     console.log(`Servidor http escuchando en el puerto ${server.address().port}`)
 })
 server.on("error", error => console.log(`Error en servidor ${error}`))
